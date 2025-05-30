6ee6c1e903ce560dfed7087ea156276711eb87ab
Ron Darnell <60163670+rdarnell55@users.noreply.github.com>
2025-05-30 11:12:43 -0500

    Fix: bind to Heroku port for deployment

--- app/app.py
+++ app/app.py
@@ -85,4 +85,5 @@ def predict():
 
 # Run the Flask application
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host="0.0.0.0", port=port)
